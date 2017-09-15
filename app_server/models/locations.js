25217dfa9518590247e64d737f66934ae3323d9a
luongkieu <kieuluonga5@gmail.com>
2017-09-15 16:17:33 -0700

    erro loc8r

---
+++ app_server/models/locations.js
@@ -0,0 +1,24 @@
+var mongoose = require( 'mongoose' );
+var openingTimeSchema = new mongoose.Schema({
+
+	days: {type: String, require: true},
+	opening: String,
+	closing: String,
+	closed: {type: Boolean, require: true}
+});
+var reviewSchema = new mongoose.Schema({
+	author: String,
+	rating: {type: Number, require: true, min: 0, max: 5},
+	reviewText: String,
+	createdOn: {type: Date, "default": Date.now}
+
+});
+var locationSchema = new mongoose.Schema({
+    name: {type: String, require: true},
+    address: String,
+    rating: {type: Number, "default": 0, min: 0, max: 5},
+    facilities: [String]
+    coords: {type: [Number], index: '2dsphere'},
+    openingTimes: [openingTimeSchema],
+    reviews: [reviewSchema]
+ });
